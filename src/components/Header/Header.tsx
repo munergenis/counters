ef4302d04b2d104e8afaf3b3b67d6b7312d7f119
munergenis <munergenis@gmail.com>
2025-05-06 01:57:41 +0200

    wip

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Title } from '../Title';
 export const Header = () => {
   return (
     <Title>
-      Counter
+      Counters
       <div className="text-xs text-right bg-a mber-200">by Mersio</div>
     </Title>
   );
